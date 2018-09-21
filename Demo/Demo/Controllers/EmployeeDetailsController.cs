a4bcc916c3cffc6ddfd23d6fb3a23662cc9e46de
ads <ads@IN-LT70343>
2018-09-21 09:39:04 +0530

    First Change

---
+++ Demo/Demo/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,26 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class EmployeeDetailsController : Controller
+    {
+        // GET: EmployeeDetails
+        public ActionResult Details()
+        {
+            EmployeeDetails employee = new EmployeeDetails()
+            {
+                EmployeeIDValue = 101,
+                FirstNameValue = "Ananya",
+                LastNameValue = "Bhavana",
+                SalaryValue = 200000
+
+            };
+            return View(employee);
+        }
+    }
+}
---
+++ Demo/Demo/Models/EmployeeDetails.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class EmployeeDetails
+    {
+        long EmployeeID, Salary;
+        string FirstName, LastName;
+        
+
+      
+        public long EmployeeIDValue
+        {
+            get
+            {
+                return EmployeeID;
+            }
+            set
+            {
+                EmployeeID = value;
+            }
+        }
+        public long SalaryValue
+        {
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                Salary = value;
+            }
+        }
+        public string FirstNameValue
+        {
+            get
+            {
+                return FirstName;
+            }
+            set
+            {
+                FirstName = value;
+            }
+        }
+        public string LastNameValue
+        {
+            get
+            {
+                return LastName;
+            }
+            set
+            {
+                LastName = value;
+            }
+        }
+        
+
+    }
+}
